18d534ec36a1c483a279e19eb3bacaf9cb56bbdc
Mar Devs <marah.mh50@gmail.com>
2025-09-19 14:33:55 +0300

    adding game logic

--- script.js
+++ script.js
@@ -26,68 +26,105 @@ const BoardGame = function(){
             case 0:
                 let index0 = prompt(`It's ${Player.Player1Name}'s turn`)
                 player1Turn = true
-                let index0Move = [index0, "Player1"]
+                let index0Move = [index0, Player.Player1Name]
                 boardGameEnteries.push(index0Move)
                 break;
             case 1:
                 let index1 = prompt(`It's ${Player.Player2Name}'s turn`)
                 player2Turn = true
-                let index1Move = [index1, "Player2"]
+                let index1Move = [index1, Player.Player2Name]
                 boardGameEnteries.push(index1Move)
                 break;
             case 2:
                 let index2 = prompt((`It's ${Player.Player1Name}'s turn`))
                 player1Turn = true
-                let index2Move = [index2, "Player1"]
+                let index2Move = [index2, Player.Player1Name]
                 boardGameEnteries.push(index2Move)
+                if ((boardGameEnteries[0][0] === boardGameEnteries[1][0]) && (boardGameEnteries[2][0] === boardGameEnteries[1][0])){
+                    console.log(`${boardGameEnteries[0][1]} wins!`)
+                    i = 9
+                }
                 break;
             case 3:
                 let index3 = prompt(`It's ${Player.Player2Name}'s turn`)
-                boardGameEnteries.push(index3)
-                player1Turn = true
-                player2Turn = false
+                player2Turn = true
+                let index3Move = [index3, Player.Player2Name]
+                boardGameEnteries.push(index3Move)
                 break;
             case 4:
                 let index4 = prompt(`It's ${Player.Player1Name}'s turn`)
-                boardGameEnteries.push(index4)
-                player1Turn = false
-                player2Turn = true
+                player1Turn = true
+                let index4Move = [index4, Player.Player1Name]
+                boardGameEnteries.push(index4Move)
+                
                 break;
             case 5:
                 let index5 = prompt(`It's ${Player.Player2Name}'s turn`)
-                boardGameEnteries.push(index5)
-                player1Turn = true
-                player2Turn = false
+                player2Turn = true
+                let index5Move = [index5, Player.Player2Name]
+                boardGameEnteries.push(index5Move)
+                if ((boardGameEnteries[0][0] === boardGameEnteries[1][0]) && (boardGameEnteries[2][0] === boardGameEnteries[1][0])){
+                    console.log(`${boardGameEnteries[0][1]} wins!`)
+                    i = 9 
+                }
+                else if ((boardGameEnteries[3][0] === boardGameEnteries[4][0]) && (boardGameEnteries[5][0] === boardGameEnteries[4][0])){
+                    console.log(`${boardGameEnteries[3][1]} wins!`)
+                    i = 9
+                }
+                
                 break;
             case 6:
                 let index6 = prompt(`It's ${Player.Player1Name}'s turn`)
-                boardGameEnteries.push(index6)
-                player1Turn = false
                 player2Turn = true
+                let index6Move = [index6, Player.Player1Name]
+                boardGameEnteries.push(index6Move)
+                if ((boardGameEnteries[0][0] === boardGameEnteries[3][0]) && (boardGameEnteries[6][0] === boardGameEnteries[3][0])){
+                    console.log(`${boardGameEnteries[0][1]} wins!`)
+                    i = 9
+                }
+                else if ((boardGameEnteries[2][0] === boardGameEnteries[4][0]) && (boardGameEnteries[6][0] === boardGameEnteries[4][0])){
+                    console.log(`${boardGameEnteries[2][1]} wins!`)
+                    i = 9
+                }
                 break;
             case 7:
                 let index7 = prompt(`It's ${Player.Player2Name}'s turn`)
-                boardGameEnteries.push(index7)
-                player1Turn = true
-                player2Turn = false
+                player2Turn = true
+                let index7Move = [index7, Player.Player2Name]
+                boardGameEnteries.push(index7Move)
+                if ((boardGameEnteries[1][0] === boardGameEnteries[4][0]) && (boardGameEnteries[7][0] === boardGameEnteries[4][0])){
+                    console.log(`${boardGameEnteries[0][1]} wins!`)
+                    i = 9
+                }
+                else if ((boardGameEnteries[2][0] === boardGameEnteries[4][0]) && (boardGameEnteries[7][0] === boardGameEnteries[4][0])){
+                    console.log(`${boardGameEnteries[0][2]} wins!`)
+                    i = 9
+                }
                 break;
             case 8:
                 let index8 = prompt(`It's ${Player.Player1Name}'s turn`)
-                boardGameEnteries.push(index8)
-                player1Turn = false
-                player2Turn = false
+                player1Turn = true
+                let index8Move = [index8, Player.Player1Name]
+                boardGameEnteries.push(index8Move)
+                if ((boardGameEnteries[0][0] === boardGameEnteries[4][0]) && (boardGameEnteries[8][0] === boardGameEnteries[4][0])){
+                    console.log(`${boardGameEnteries[0][1]} wins!`)
+                    i = 9
+                }
+
+                else if ((boardGameEnteries[2][0] === boardGameEnteries[5][0]) && (boardGameEnteries[8][0] === boardGameEnteries[5][0])){
+                    console.log(`${boardGameEnteries[2][1]} wins!`)
+                    i = 9
+                }
+                else if ((boardGameEnteries[6][0] === boardGameEnteries[7][0]) && (boardGameEnteries[8][0] === boardGameEnteries[7][0])){
+                    console.log(`${boardGameEnteries[6][1]} wins!`)
+                    i = 9
+                }
                 break;
 
             default:
                 prompt("Game Over!")
         }
-
-        if(boardGameEnteries[0] && boardGameEnteries[1] && boardGameEnteries[2] === 'X' || boardGameEnteries[0] && boardGameEnteries[1] && boardGameEnteries[2] === 'O'){
-            
-        }
-
     }
-
 
     return boardGameEnteries
 
